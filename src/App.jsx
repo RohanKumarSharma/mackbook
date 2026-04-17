e93d923213407a2bcb1d3be97b4e901dc28ef07c
rohankumarsharma <rk604391@gmail.com>
2026-04-17 11:19:49 +0530

    Implement Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from 'gsap';
-import  { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx";
 import Performance from "./components/Performance.jsx";
 import Features from "./components/Features.jsx";
@@ -12,18 +12,18 @@ import Footer from "./components/Footer.jsx";
 gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
-  return (
-    <main>
-      <NavBar/>
-      <Hero />
-      <ProductViewer />
-      <Showcase />
-      <Performance />
-      <Features />
-      <Highlights />
-      <Footer />
-    </main>
-  )
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
 }
 
-export default App;
+export default App
